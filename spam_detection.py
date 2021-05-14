6e4524f8034e9614f3f71ddff397171a6ac5c0e5
srikanth-sri756 <59730561+srikanth-sri756@users.noreply.github.com>
2021-05-14 11:02:00 +0530

    Add files via upload

---
+++ spam_detection.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB, GaussianNB
+from sklearn import svm
+from sklearn.model_selection import GridSearchCV
+
+##Step1: Load Dataset
+
+##Step2: Split in to Training and Test Data
+
+##Step3: Extract Features
+
+##Step4: Build a model
+
+##Step5: Test Accuracy
+
+
+
+
